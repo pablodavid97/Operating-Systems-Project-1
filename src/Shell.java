45b1c2f3ebdaf0c446575a5244f3b0ee4b66ce58
Pablo <pablodavidllanes@gmail.com>
2019-02-09 22:18:19 -0500

    Added command history and cases for command parser

--- src/Shell.java
+++ src/Shell.java
@@ -1,18 +1,72 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Shell {
+    private static LinkedList<String> commandHistory;
+
     public Shell(){
+        commandHistory = new LinkedList<>();
     }
 
-    public static void commandParser(String commandLine){
+    public void commandParser(String commandLine){
         String[] commands = commandLine.split(" ");
-        System.out.println(commands.length);
 
         for(String command : commands){
-            System.out.println(command);
+
+            switch (command){
+                case "ls":
+                    commandHistory.add(command);
+                    break;
+                case "cd":
+                    commandHistory.add(command);
+                    break;
+                case "echo":
+                    commandHistory.add(command);
+                    break;
+                case "ping":
+                    commandHistory.add(command);
+                    break;
+                case "ipconfig":
+                    commandHistory.add(command);
+                    break;
+                case "ifconfig":
+                    commandHistory.add(command);
+                    break;
+                case "history":
+                    getCommandHistory();
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.err.println("Comando Invalido!");
+            };
+        }
+    }
+
+    public void getCommandHistory(){
+        Iterator<String> iterator = commandHistory.descendingIterator();
+
+        int i = 0;
+        while(iterator.hasNext() && i < 20){
+            i++;
+            System.out.println(i + " " + iterator.next());
         }
     }
 
     public static void main(String[] args) {
-        commandParser("Hello World!");
+        Shell shell = new Shell();
+        Scanner input = new Scanner(System.in);
+        String commandLine = "";
+
+        do{
+            System.out.println(">");
+
+            commandLine = input.nextLine();
+
+            shell.commandParser(commandLine);
+        }
+        while(!commandLine.equals("exit"));
     }
 
 }
